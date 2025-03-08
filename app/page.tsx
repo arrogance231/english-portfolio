6d9a975a7e661bdb79a5089cfc97c6044ffd76e9
arrogance231 <alchemyofjhine@gmail.com>
2025-03-08 15:51:20 +0800

    fix: fixing of mobile text p2

--- app/page.tsx
+++ app/page.tsx
@@ -6,43 +6,43 @@ const line1 = [
   {
     text: "Specialized English Program I",
     className:
-      "text-[16px] sm:text-[24px] md:text-[28px] xl:text-[40px] font-semibold font-poppins leading-none text-white",
+      "text-[16px] sm:text-[24px] md:text-[28px] xl:text-[40px] font-semibold font-poppins text-white",
     duration: 0,
   },
 ];
 
 const line2_1Mobile = [
   {
-    text: "Digital",
+    text: "DIGITAL",
     className:
-      "text-[64px] md:text-[52px] font-bold font-poppins leading-none text-white w-full",
+      "text-[50px] md:text-[52px] font-bold font-poppins text-white w-full",
     duration: 0.5,
   },
 ];
 
 const line2_2Mobile = [
   {
-    text: "Reading",
+    text: "READING",
     className:
-      "text-[64px] md:text-[52px] font-bold font-poppins leading-none text-white w-full",
+      "text-[50px] md:text-[52px] font-bold font-poppins text-white w-full",
     duration: 0.5,
   },
 ];
 
 const line2Desktop = [
   {
-    text: "Digital Reading",
+    text: "DIGITAL READING",
     className:
-      "md:text-[52px] lg:text-[108px] font-bold font-poppins leading-none text-white w-full md:w-[80vw]",
+      "md:text-[52px] lg:text-[108px] font-bold font-poppins text-white w-full md:w-[80vw]",
     duration: 0.5,
   },
 ];
 
 const line3 = [
   {
-    text: "Portfolio",
+    text: "PORTFOLIO",
     className:
-      "text-[64px] sm:text-[40px] md:text-[52px] lg:text-[108px] font-bold font-poppins leading-none text-[#6EBEFF]",
+      "text-[50px] sm:text-[40px] md:text-[52px] lg:text-[108px] font-bold font-poppins  text-[#6EBEFF]",
     duration: 0.5,
   },
 ];
@@ -60,10 +60,10 @@ export default function Home() {
         />
         <div className="absolute inset-0 flex flex-col md:flex-row">
           <SidebarDemo />
-          <div className=" w-screen flex flex-col items-start justify-center p-4 pb-44 mt-20 md:mt-0 md:p-16 lg:p-32 xl:p-64 ml-2 md:ml-10 lg:ml-2 h-full">
+          <div className="z-0 flex flex-col items-start justify-center p-4 pb-44 mt-20 md:mt-0 md:p-16 lg:p-32 xl:p-64 ml-2 md:ml-10 lg:ml-2 h-full">
             <TypewriterEffectSmooth
               words={line1}
-              className={`${line1[0].className} xl:-mb-1`}
+              className={`${line1[0].className} xl:-mb-9`}
               delay={line1[0].duration}
             />
             <div className="block md:hidden">
@@ -81,27 +81,27 @@ export default function Home() {
             <div className="hidden md:block">
               <TypewriterEffectSmooth
                 words={line2Desktop}
-                className={`${line2Desktop[0].className} xl:-mb-3`}
+                className={`${line2Desktop[0].className} xl:-mb-14`}
                 delay={line2Desktop[0].duration}
               />
             </div>
             <TypewriterEffectSmooth
               words={line3}
-              className={`${line3[0].className} xl:-mb-3`}
+              className={`${line3[0].className} xl:-mb-12`}
               delay={line3[0].duration}
             />
+            <div className="xl:px-64 absolute bottom-0 right-0 z-50 flex flex-col md:flex-row justify-evenly items-center xl:py-16 space-y-4 md:space-y-0 md:space-x-4 w-full xl:mr-12">
+              <div className="bg-[#133B5C]/45 text-white text-center font-poppins p-2 sm:p-4 rounded-2xl shadow-md w-3/4 sm:w-1/4 border-[1.5px] border-white">
+                Arjhine A. Ty
+              </div>
+              <div className="bg-[#133B5C]/45 text-white text-center font-poppins p-2 sm:p-4 rounded-2xl shadow-md w-3/4 sm:w-1/4 border-[1.5px] border-white">
+                LinkedIn/Arro
+              </div>
+              <div className="bg-[#133B5C]/45 text-white text-center font-poppins p-2 sm:p-4 rounded-2xl shadow-md w-3/4 sm:w-1/4 border-[1.5px] border-white">
+                Portfolio
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="absolute bottom-0 left-0 right-0 flex flex-col md:flex-row justify-evenly items-center p-4 space-y-4 md:space-y-0 md:space-x-4">
-        <div className="bg-[#133B5C]/45 text-white text-center font-poppins p-2 sm:p-4 rounded-3xl shadow-md w-3/4 sm:w-1/4 border-[1.5px] border-white">
-          Arjhine A. Ty
-        </div>
-        <div className="bg-[#133B5C]/45 text-white text-center font-poppins p-2 sm:p-4 rounded-3xl shadow-md w-3/4 sm:w-1/4 border-[1.5px] border-white">
-          LinkedIn/Arro
-        </div>
-        <div className="bg-[#133B5C]/45 text-white text-center font-poppins p-2 sm:p-4 rounded-3xl shadow-md w-3/4 sm:w-1/4 border-[1.5px] border-white">
-          Portfolio
         </div>
       </div>
     </div>

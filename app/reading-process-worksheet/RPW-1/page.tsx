58b826215c93defecb7068930502ec63810e7693
arrogance231 <alchemyofjhine@gmail.com>
2025-03-10 13:19:06 +0800

    feat: added template for Reading Process Worksheet 1 and 2

--- app/reading-process-worksheet/RPW-1/page.tsx
+++ app/reading-process-worksheet/RPW-1/page.tsx
@@ -1,56 +1,127 @@
-import Image from "next/image";
 import { SidebarDemo } from "@/components/NavBar";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative h-screen w-screen flex overflow-hidden">
-      <div className="relative h-full w-full">
-        <Image
-          src="/bg.svg"
-          alt="bg"
-          layout="fill"
-          objectFit="cover"
-          className="h-full w-full"
-        />
-        <div className="absolute inset-0 flex flex-col md:flex-row">
+      <Image
+        src="/bg.svg"
+        alt="bg"
+        layout="fill"
+        objectFit="cover"
+        className="absolute h-full w-full"
+      />
+      <div className="relative h-full w-full overflow-hidden">
+        <div className="absolute inset-0 flex flex-col xl:px-[100px]">
           <SidebarDemo />
-          <div className="animate-appearFromCenter text-white xl:mx-32 xl:my-8 md:mx-32 mx-5 my-4 p-8 rounded-xl md:p-8 xl:w-full xl:h-full overflow-y-scroll animate-gradient bg-gradient-to-t from-[#133B5C]/50 to-[#FFA6AF]/50">
+          <div className="animate-appearFromCenter text-white xl:mx-auto xl:my-8 md:mx-32 mx-5 my-4 p-8 rounded-xl md:p-8 xl:w-full xl:h-full overflow-y-auto animate-gradient bg-gradient-to-t from-[#133B5C]/50 to-[#FFA6AF]/50">
+            <h1 className="text-4xl font-bold mb-8">
+              Reading Process Worksheet 1
+            </h1>
             <div className="grid h-full w-full rounded-x">
-              <Image
-                src="/RPW1/S1P1.svg"
-                alt="p1"
-                width={0}
-                height={0}
-                className="w-full h-full"
-              ></Image>
-              <Image
-                src="/RPW1/S1P2.svg"
-                alt="p1"
-                width={0}
-                height={0}
-                className="w-full h-full"
-              ></Image>
-              <Image
-                src="/RPW1/S1P3.svg"
-                alt="p1"
-                width={0}
-                height={0}
-                className="w-full h-full"
-              ></Image>
-              <Image
-                src="/RPW1/S1P4.svg"
-                alt="p1"
-                width={0}
-                height={0}
-                className="w-full h-full"
-              ></Image>
-              <Image
-                src="/RPW1/S1P5.svg"
-                alt="p1"
-                width={0}
-                height={0}
-                className="w-full h-full"
-              ></Image>
+              <section className="mb-8">
+                <h2 className="text-8xl font-semibold mb-4">Pre-Reading</h2>
+                <h3 className="my-10">
+                  A. Complete the table with the headings, subheadings, and
+                  visual titles/descriptions from the text. Provide at least 2
+                  entries for each column.
+                </h3>
+                <table className="table-auto w-full mb-8">
+                  <tbody>
+                    <tr>
+                      <td className="border px-4 py-2 text-center">Headings</td>
+                      <td className="border px-4 py-2 text-center">
+                        Subheadings
+                      </td>
+                      <td className="border px-4 py-2 text-center">Visuals</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 2, Col 1</td>
+                      <td className="border px-4 py-2">Row 2, Col 2</td>
+                      <td className="border px-4 py-2">Row 2, Col 3</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <h3>
+                  B. Fill the table with information that you already know about
+                  the topic (K), you wonder about the topic (W), and you will
+                  learn after reading the text (L). Fill the L Column after
+                  reading the text. Provide at least 2 entries for each column.{" "}
+                </h3>
+                <table className="table-auto w-full mb-8">
+                  <tbody>
+                    <tr>
+                      <td className="border px-4 py-2 text-center">K (Know)</td>
+                      <td className="border px-4 py-2 text-center">
+                        W (Wonder)
+                      </td>
+                      <td className="border px-4 py-2 text-center">
+                        L (Learn)
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 2, Col 1</td>
+                      <td className="border px-4 py-2">Row 2, Col 2</td>
+                      <td className="border px-4 py-2">Row 2, Col 3</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </section>
+
+              <section className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">Reading</h2>
+                <table className="table-auto w-full mb-8">
+                  <tbody>
+                    <tr>
+                      <td className="border px-4 py-2 text-center">Column 1</td>
+                      <td className="border px-4 py-2 text-center">Column 2</td>
+                      <td className="border px-4 py-2 text-center">Column 3</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 2, Col 1</td>
+                      <td className="border px-4 py-2">Row 2, Col 2</td>
+                      <td className="border px-4 py-2">Row 2, Col 3</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 3, Col 1</td>
+                      <td className="border px-4 py-2">Row 3, Col 2</td>
+                      <td className="border px-4 py-2">Row 3, Col 3</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <table className="table-auto w-full mb-8">
+                  <tbody>
+                    <tr>
+                      <td className="border px-4 py-2 text-center">Column 1</td>
+                      <td className="border px-4 py-2 text-center">Column 2</td>
+                      <td className="border px-4 py-2 text-center">Column 3</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 2, Col 1</td>
+                      <td className="border px-4 py-2">Row 2, Col 2</td>
+                      <td className="border px-4 py-2">Row 2, Col 3</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 3, Col 1</td>
+                      <td className="border px-4 py-2">Row 3, Col 2</td>
+                      <td className="border px-4 py-2">Row 3, Col 3</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 4, Col 1</td>
+                      <td className="border px-4 py-2">Row 4, Col 2</td>
+                      <td className="border px-4 py-2">Row 4, Col 3</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Row 5, Col 1</td>
+                      <td className="border px-4 py-2">Row 5, Col 2</td>
+                      <td className="border px-4 py-2">Row 5, Col 3</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </section>
+              <section>
+                <h2 className="text-2xl font-semibold mb-4">Post-Reading</h2>
+                <p>Content for Post-Reading section...</p>
+              </section>
             </div>
           </div>
         </div>
